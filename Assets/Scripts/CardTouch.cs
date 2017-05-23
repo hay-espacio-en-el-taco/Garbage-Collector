107c70f1981e992cff8e9f9359ed4887f2eee57c
Cloud FullBuster <lordzero0000@gmail.com>
2017-05-23 01:08:41 -0500

    Drag and Drop

---
+++ Assets/Scripts/CardTouch.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardTouch : MonoBehaviour {
+
+    private SpringJoint2D spring;
+
+
+    void Awake()
+    {
+
+        spring = this.gameObject.GetComponent<SpringJoint2D>(); 
+        //"spring" is the SpringJoint2D component that I added to my object
+
+        spring.connectedAnchor = gameObject.transform.position;
+        //i want the anchor position to start at the object's position
+
+        Debug.Log("I'm ready to be drag!");
+
+    }
+
+
+    void OnMouseDown()
+    {
+
+        spring.enabled = true;
+        Debug.Log("Clicked!");
+        //I'm reactivating the SpringJoint2D component each time I'm clicking on my object becouse I'm disabling it after I'm releasing the mouse click so it will fly in the direction i was moving my mouse
+
+    }
+
+
+    void OnMouseDrag()
+    {
+
+        if (spring.enabled == true)
+        {
+
+            Vector2 cursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //getting cursor position
+            Debug.Log(cursorPosition);
+
+            //spring.connectedAnchor = cursorPosition;
+            this.gameObject.transform.Translate(cursorPosition);
+            //the anchor get's cursor's position
+
+
+        }
+    }
+
+
+    void OnMouseUp()
+    {
+
+        spring.enabled = false;//disabling the spring component
+        Debug.Log("No more dragging");
+
+    }
+
+    void Update()
+    {
+        // TODO: stuff
+    }
+}
---
+++ Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class DragAndDrop : MonoBehaviour
+{
+    public float moveSpeed;
+    public float offset = 0.05f;
+    private bool following;
+    // Use this for initialization
+    void Start()
+    {
+        following = false;
+        offset += 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && ((Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).magnitude <= offset))
+        {
+            following = true;
+        } else if (Input.GetMouseButtonUp(0))
+        {
+            following = false;
+        }
+        if (following)
+        {
+            transform.position = Vector2.Lerp(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), moveSpeed);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "PlaySurface")
+        {
+            Debug.Log("Activated!");
+        }
+
+    }
+}
